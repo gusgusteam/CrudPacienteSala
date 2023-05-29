c8fecc1505c57c02d24824883e7ac33e462f5702
gusgusteam <109000871+gusgusteam@users.noreply.github.com>
2023-05-29 13:01:24 -0800

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Paciente Sala</title>
-    <link rel="stylesheet" href="{{asset('/librerias/bootstrap.min.css')}}" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('/librerias/bootstrap.min.css')}}"  crossorigin="anonymous">
     
     
 </head>
